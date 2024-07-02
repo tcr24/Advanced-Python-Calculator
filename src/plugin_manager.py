101a2c294c83b739d822846bac3f9c41727174f3
tcr24 <tcr24@njit.edu>
2024-07-01 20:52:12 -0400

    Add docstrings to PluginManager

--- src/plugin_manager.py
+++ src/plugin_manager.py
@@ -2,16 +2,20 @@ import importlib
 import os
 
 class PluginManager:
+    """Manager for loading and handling plugins."""
     def __init__(self, plugin_folder='plugins'):
+        """Initialize the plugin manager with the given folder."""
         self.plugin_folder = plugin_folder
         self.plugins = {}
 
     def load_plugins(self):
+        """Load all plugins from the plugin folder."""
         for filename in os.listdir(self.plugin_folder):
-            if filename.endswith('.py'):
+            if filename.endswith('.py') and filename != '__init__.py':
                 plugin_name = filename[:-3]
                 module = importlib.import_module(f'{self.plugin_folder}.{plugin_name}')
                 self.plugins[plugin_name] = module
 
     def get_plugins(self):
+        """Get the loaded plugins."""
         return self.plugins
